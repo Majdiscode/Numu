9314cbbff57ebfafd60686f11de6f12033a20f42
Majdiscode <majdiscode@gmail.com>
2025-11-12 14:01:25 -0800

    Initial Commit

---
+++ Numu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Numu
+//
+//  Created by Majd Iskandarani on 11/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Numu/NumuApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumuApp.swift
+//  Numu
+//
+//  Created by Majd Iskandarani on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NumuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
